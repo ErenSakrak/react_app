5e65d30c5895e641476ce277068f503433fcf86e
ErenSakrak <erensakrak@outlook.com>
2024-08-21 13:54:22 +0300

    json updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Blog from "./pages/Blog";
-import Hakkinda from "./pages/Hakkinda";
-import Iletisim from "./pages/Iletisim";
-import Anasayfa from "./pages/Anasayfa";
-import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Anasayfa />} />
-          <Route path="blog" element={<Blog />} />
-          <Route path="hakkinda" element={<Hakkinda />} />
-          <Route path="iletisim" element={<Iletisim />} />
-        </Routes>
-      </BrowserRouter>
+        <h2>App Sayfası</h2>
       </header>
     </div>
   );
